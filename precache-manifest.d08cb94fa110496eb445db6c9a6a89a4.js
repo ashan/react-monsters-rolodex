b4582eafa50288152ad56b8a213927c987d08f59
ashan <k.a.a.dilantha@gmail.com>
2019-11-26 17:00:14 +1300

    Updates

---
+++ precache-manifest.d08cb94fa110496eb445db6c9a6a89a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4206082bd8308318725c53f1ba3a55",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b2cb2ccce69029dd88b",
+    "url": "/react-monsters-rolodex/static/css/main.8174fba3.chunk.css"
+  },
+  {
+    "revision": "5e404e4e23ac080fa4fb",
+    "url": "/react-monsters-rolodex/static/js/2.e4bd5644.chunk.js"
+  },
+  {
+    "revision": "5b2cb2ccce69029dd88b",
+    "url": "/react-monsters-rolodex/static/js/main.844c43d1.chunk.js"
+  },
+  {
+    "revision": "e0faa840671b71440f1a",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.a4f32280.js"
+  }
+]);
